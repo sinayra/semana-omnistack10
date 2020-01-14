f3ca87ab9c6e42495cdd8cff101954f4bddf409c
sinayra <nayra@hotmail.co.jp>
2020-01-14 20:49:10 -0300

    Connecting to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 app.listen(3333);
 app.use(express.json());
+
+mongoose.connect('mongodb://adm:adm@localhost:27017/omnistack', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to localhost');
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
 
 //Métodos HTTP: GET, POST, PUT, DELETE
 
@@ -11,5 +30,5 @@ app.use(express.json());
 // Body: request.body (dados para criação ou alteração de um registro)
 
 app.get('/', (request, response) => {
-    return response.json({message: 'Hello World'});
+    return response.json({ message: 'Hello World' });
 });
