089a3bacc5ceb4a687181d80123ea69b947b616a
sinayra <nayra@hotmail.co.jp>
2020-01-14 21:29:44 -0300

    Insert data

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const {Router} = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
@@ -9,8 +11,22 @@ const routes = Router();
 // Route params: request.params (identificar um recurso na alteração ou remoção)
 // Body: request.body (dados para criação ou alteração de um registro)
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
+routes.post('/devs', async (request, response) => {
+    const { github_username, techs } = request.body;
+    const apiGitResponse =  await axios.get(`https://api.github.com/users/${github_username}`);
+    const name = apiGitResponse.data.name || apiGitResponse.data.login;
+    const avatar_url = apiGitResponse.data.avatar_url;
+    const bio = apiGitResponse.data.bio || '';
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray
+    });
+    return response.json(dev);
 });
 
 module.exports = routes;
