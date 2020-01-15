32c2459603adad18dd56f0219b2099d817eaf2fa
sinayra <nayra@hotmail.co.jp>
2020-01-14 21:45:58 -0300

    Remove duplicate insert

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -4,27 +4,31 @@ const Dev = require('../models/Dev');
 module.exports = {
     async store (request, response)  {
         const { github_username, techs, latitude, longitude } = request.body;
-    
-        const apiGitResponse =  await axios.get(`https://api.github.com/users/${github_username}`);
-        const name = apiGitResponse.data.name || apiGitResponse.data.login;
-        const avatar_url = apiGitResponse.data.avatar_url;
-        const bio = apiGitResponse.data.bio || '';
+
+        let dev = await Dev.findOne({ github_username });
+
+        if(!dev){
+            const apiGitResponse =  await axios.get(`https://api.github.com/users/${github_username}`);
+            const name = apiGitResponse.data.name || apiGitResponse.data.login;
+            const avatar_url = apiGitResponse.data.avatar_url;
+            const bio = apiGitResponse.data.bio || '';
+            
+            const techsArray = techs.split(',').map(tech => tech.trim());
+            
+            const location = {
+                type: 'Point',
+                coordinates:[longitude, latitude]
+            }
         
-        const techsArray = techs.split(',').map(tech => tech.trim());
-        
-        const location = {
-            type: 'Point',
-            coordinates:[longitude, latitude]
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            });
         }
-    
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location
-        });
         return response.json(dev);
     }
 }
