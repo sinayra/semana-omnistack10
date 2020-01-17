22ef10458aeb1ffd073fb2911ac8cac9b32769e1
sinayra <nayra@hotmail.co.jp>
2020-01-16 22:36:38 -0300

    Restructure folders and files

--- web/src/App.js
+++ web/src/App.js
@@ -10,31 +10,12 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
-function App() {
-    const [latitude, setLatitude] = useState('');
-    const [longitude, setLongitude] = useState('');
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
 
-    const[github_username, setGitHubUsername] = useState('');
-    const[techs, setTechs] = useState('');
+function App() {
 
     const [devs, setDevs] = useState([]);
-
-    useEffect(() => {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const {latitude, longitude} = position.coords;
-
-          setLatitude(latitude);
-          setLongitude(longitude);
-        },
-        (err) => {
-          console.log(err);
-        },
-        {
-          timeout: 30000,
-        }
-      );
-    }, []);
 
     useEffect(() => {
       async function loadDevs(){
@@ -45,18 +26,8 @@ function App() {
 
       loadDevs();
     }, []);
-    async function handleAddDev(e){
-      e.preventDefault();
-
-      const response = await api.post('/devs', {
-        github_username,
-        techs,
-        latitude,
-        longitude
-      });
-
-      setGitHubUsername('');
-      setTechs('');
+    async function handleAddDev(data){
+      const response = await api.post('/devs', data);
 
       setDevs([...devs, response.data]);
     }
@@ -65,78 +36,14 @@ function App() {
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input 
-              name="github_username" 
-              id="github_username" 
-              required 
-              value={github_username}
-              onChange={e => setGitHubUsername(e.target.value)}
-            />
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input 
-              name="techs" 
-              id="techs" 
-              required 
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input 
-                type ="number"
-                step="any" 
-                name="latitude" 
-                id="latitude" 
-                required 
-                value={latitude} 
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input 
-                type ="number"
-                step="any" 
-                name="longitude" 
-                id="longitude" 
-                required 
-                value={longitude} 
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
 
       <main>
         <ul>
-          {
-            devs.map(
-              dev => (
-                <li key={dev._id} className="dev-item">
-                <header>
-                  <img src={dev.avatar_url} alt={dev.name} />
-                  <div className="user-info">
-                    <strong>{dev.name}</strong>
-                    <span>{dev.techs.join(',')}</span>
-                  </div>
-                </header>
-                <p>{dev.bio}</p>
-                <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no GitHub</a>
-              </li>
-              )
-            )
-          }
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
