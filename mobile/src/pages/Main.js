737755a86c61c98396160398710ea4839a74a60c
sinayra <nayra@hotmail.co.jp>
2020-01-18 16:17:08 -0300

    Showing form

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons'
 
 function Main({ navigation }){
     const[currentRegion, setCurrentRegion] = useState(null);
@@ -34,22 +35,37 @@ function Main({ navigation }){
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={ {latitude: -15.7508826, longitude: -47.8974287} }>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={ {latitude: -15.7508826, longitude: -47.8974287} }>
 
-                <Image style={ styles.avatar } source={{uri: 'https://avatars2.githubusercontent.com/u/4048570?s=460&v=4'}}/>
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'sinayra' });
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Nome</Text>
-                        <Text style={styles.devBio}>Bio</Text>
-                        <Text style={styles.devTechs}>Techs</Text>
-                    </View>
-                </Callout>
+                    <Image style={ styles.avatar } source={{uri: 'https://avatars2.githubusercontent.com/u/4048570?s=460&v=4'}}/>
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'sinayra' });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Nome</Text>
+                            <Text style={styles.devBio}>Bio</Text>
+                            <Text style={styles.devTechs}>Techs</Text>
+                        </View>
+                    </Callout>
 
-            </Marker>
-        </MapView>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput 
+                    style={styles.searchInput}
+                    placeholder="Buscar debs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => {}}style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF" />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -82,6 +98,41 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+        elevation: 2
+    },
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8e4dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15
     }
 })
 
